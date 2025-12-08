01bdee141cb38b45d1301f3fec4aefd041dc864b
Aditya Reddy <adityaareddyy@gmail.com>
2025-12-08 15:14:16 +0530

    Update db.js

--- backend-node/db.js
+++ backend-node/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const connectDB = async () =>{
     try {
         mongoose.connect("mongodb://127.0.0.1:27017/voiceJournalDB");
-        console.log("Connect to MongoDB");
+        console.log("Connected to MongoDB");
     } catch (error) {
         console.log("DID NOT Connect to MONGO: ",error);
     }
